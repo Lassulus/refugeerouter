2617e507eea8ae686f0d4a13bc27fed89d49692d
frank honza <frank.honza@dcso.de>
2022-03-25 13:31:36 +0100

    cleanup

--- refugeerouter/forms.py
+++ refugeerouter/forms.py
@@ -3,8 +3,6 @@ from refugeerouter.models import Group
 
 
 class UpdateGroupForm(forms.ModelForm):
-    signature = forms.CharField(required=False, widget=forms.TextInput(attrs={"readonly": "readonly"}))
-
     class Meta:
         model = Group
         fields = [
